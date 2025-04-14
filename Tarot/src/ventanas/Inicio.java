533d6f67562147d3a413284b0bd3692480c1ba00
René Rodríguez Martínez <renerodrim@gmail.com>
2025-04-14 13:41:31 +0200

    Primera fase terminada

--- Tarot/src/ventanas/Inicio.java
+++ Tarot/src/ventanas/Inicio.java
@@ -4,17 +4,36 @@
  */
 package ventanas;
 
+import java.util.ArrayList;
+import java.util.Random;
+import javax.swing.ImageIcon;
+
 /**
  *
- * @author usuario
+ * @author René Rodríguez Martínez
  */
 public class Inicio extends javax.swing.JFrame {
 
-    /**
-     * Creates new form Inicio
-     */
+   
+    
+    //lista con las rutas de todas las cartas
+    private ArrayList<String> cartas ;
+    //cantidad de cartas en la carpeta
+    private final int CANTIDAD_DE_CARTAS=22; 
+    //objeto random con el que vamos a randomizar sacar cartas aleatorias
+    private Random random;
+    //para saber si se ha revelado la carta antes para dar la vuelta a la carta
+    private Boolean reverso;
+    
     public Inicio() {
         initComponents();
+        jLabel1.setIcon(new ImageIcon(getClass().getResource("/img/tarot_cover_red.png")));
+        jLabel2.setIcon(new ImageIcon(getClass().getResource("/img/tapete.png")));       
+        cartas = new ArrayList<>();
+        random=new Random();
+        reverso=true;
+        cargarCartas();
+
     }
 
     /**
@@ -26,21 +45,31 @@ public class Inicio extends javax.swing.JFrame {
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
     private void initComponents() {
 
-        setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
+        jLabel1 = new javax.swing.JLabel();
+        jLabel2 = new javax.swing.JLabel();
 
-        javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
-        getContentPane().setLayout(layout);
-        layout.setHorizontalGroup(
-            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 400, Short.MAX_VALUE)
-        );
-        layout.setVerticalGroup(
-            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 300, Short.MAX_VALUE)
-        );
+        setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
+        setPreferredSize(new java.awt.Dimension(500, 500));
+        getContentPane().setLayout(new org.netbeans.lib.awtextra.AbsoluteLayout());
+
+        jLabel1.setName(""); // NOI18N
+        jLabel1.setPreferredSize(new java.awt.Dimension(230, 356));
+        jLabel1.addMouseListener(new java.awt.event.MouseAdapter() {
+            public void mouseClicked(java.awt.event.MouseEvent evt) {
+                jLabel1MouseClicked(evt);
+            }
+        });
+        getContentPane().add(jLabel1, new org.netbeans.lib.awtextra.AbsoluteConstraints(140, 100, -1, -1));
+
+        jLabel2.setPreferredSize(new java.awt.Dimension(500, 500));
+        getContentPane().add(jLabel2, new org.netbeans.lib.awtextra.AbsoluteConstraints(0, 0, -1, -1));
 
         pack();
     }// </editor-fold>//GEN-END:initComponents
+
+    private void jLabel1MouseClicked(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_jLabel1MouseClicked
+        mostrarCarta();
+    }//GEN-LAST:event_jLabel1MouseClicked
 
     /**
      * @param args the command line arguments
@@ -78,5 +107,28 @@ public class Inicio extends javax.swing.JFrame {
     }
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JLabel jLabel1;
+    private javax.swing.JLabel jLabel2;
     // End of variables declaration//GEN-END:variables
+    
+    //metodo que añade las rutas de las imagenes al arraylist
+    private void cargarCartas() {
+         for (int i = 1; i <= CANTIDAD_DE_CARTAS; i++) {
+            cartas.add("/img-cartas/" + i + ".png");
+        }
+    }
+    
+    private void mostrarCarta(){
+      String ruta;   
+       if(reverso){
+        int i = random.nextInt(cartas.size());
+        ruta = cartas.get(i);
+        jLabel1.setIcon(new ImageIcon(getClass().getResource(ruta)));
+        reverso=false;
+       }else{
+        jLabel1.setIcon(new ImageIcon(getClass().getResource("/img/tarot_cover_red.png")));
+        reverso=true;
+       }
+    }       
+ 
 }
